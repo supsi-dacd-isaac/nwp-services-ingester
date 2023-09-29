52bf8c191aa1cc39a56367e9ea3be33f23678894
vascomedici <vasco.medici@supsi.ch>
2023-09-29 18:15:34 +0200

    save space when pickling

--- open_meteo.py
+++ open_meteo.py
@@ -46,7 +46,7 @@ def get_open_meteo_data_single_location(location: dict, dt: pd.Timestamp):
             os.makedirs(open_meteo_source_dir, exist_ok=True)
             model_path = os.path.join(open_meteo_source_dir, ens)
             os.makedirs(model_path, exist_ok=True)
-            d.to_pickle(os.path.join(model_path, f'open_meteo_forecast_{ens}_{dt.date()}.pickle'))
+            d.to_pickle(os.path.join(model_path, f'open_meteo_forecast_{ens}_{dt.date()}.zip'))
 
             tags = ['location', 'latitude', 'longitude', 'elevation', 'step', 'ensemble', 'member']
             write_to_influx(d, tags, 'open-meteo')
