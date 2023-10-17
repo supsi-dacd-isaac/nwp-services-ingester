d3421ee7a40938cf9aaae8a70cddbf9a1646c266
vascomedici <vasco.medici@supsi.ch>
2023-10-17 18:10:49 +0200

    fixed zip in rotating logger

--- zipped_time_rotating_file_handler.py
+++ zipped_time_rotating_file_handler.py
@@ -1,6 +1,8 @@
 from logging.handlers import TimedRotatingFileHandler
 import zipfile
 import os
+import time
+from glob import glob
 
 
 class ZippedTimedRotatingFileHandler(TimedRotatingFileHandler):
@@ -11,9 +13,12 @@ class ZippedTimedRotatingFileHandler(TimedRotatingFileHandler):
         # Rotate the log file first
         super().doRollover()
 
-        # Zip the previous log file
-        prev_log = self.baseFilename + ".1"
-        if os.path.exists(prev_log):
-            with zipfile.ZipFile(prev_log + ".zip", 'w', zipfile.ZIP_DEFLATED) as zf:
-                zf.write(prev_log)
-            os.remove(prev_log)
+        # get list of files which will be zipped, basically all files with the same baseFilename
+        to_zip = glob(self.baseFilename+'.*')
+        # delete files ending with .zip
+        to_zip = [f for f in to_zip if not f.endswith('.zip')]
+
+        for tz in to_zip:
+            with zipfile.ZipFile(tz + ".zip", 'w', zipfile.ZIP_DEFLATED) as zf:
+                zf.write(tz)
+            os.remove(tz)
